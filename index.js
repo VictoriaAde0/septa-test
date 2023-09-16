2b3ffede51676d798ac5b8cec47c0d98ec05f1d8
VictoriaAde0 <adejumotoria@gmail.com>
2023-09-16 08:18:02 +0100

    add assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+alert('hello world');
+document.write('hi people');
+console.log('hey')
+let age= 10
+let newage= 15
+  
+
+console.log(age+newage)
+
+
+let name1= "Ann"
+let name2=  "Tom"
+
+
+console.log(name1 + " " + name2)
+
+
+const website = "google";
+const url = "https://www." + website + ".com"
+
+
+console.log(url)
+
+const Name = "Victoria";
+const school = "Lautech"
+
+
+console.log("hello my name is" + " "  + Name + " " + "and"  +  " " + "i attended" + " " + school)
+
+// Assignment
+
+// Difference between var, let and const
+
+// var element represents the name of a variable in a mathematical expression or a programming context. It is a statement that declares a variable that can be treated as any other variable
+// let element represents another variable in javascript. It is a statement used to declare a variable that is block scoped.
+// const element declares  creates a fixed reference to a value. It does not mean the value it holds is unchangeable meaning that the variable identifier cannot be reassigned to another.
+
