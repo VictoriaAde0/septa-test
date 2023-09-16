43f54da8601fc43278c1d1560cbbdd035e59d766
VictoriaAde0 <adejumotoria@gmail.com>
2023-09-16 17:56:01 +0100

    add index.js

--- index.js
+++ index.js
@@ -36,10 +36,119 @@ console.log("hello my name is" + " "  + Name + " " + "and"  +  " " + "i attended
 // const element declares  creates a fixed reference to a value. It does not mean the value it holds is unchangeable meaning that the variable identifier cannot be reassigned to another.
 
 
-//var
-var name = "victoria";
 
-var name = "Lisa"
+let car = "toyota"
 
-console.log(name)
 
+console.log(car)
+
+
+
+function greet() {
+console.log("hello my people");
+} 
+
+greet()
+
+
+function welcome(name3){
+console.log(name3)
+}
+
+welcome("Victoria")
+welcome("Bolu")
+
+
+
+
+
+function Country1(){
+ console.log("Nigeria, NIG")
+}
+
+Country1("Nigeria, NIG")
+
+
+function Country2(){
+    console.log("Ghana, GHA")
+   }
+Country2("Ghana, GHA")
+
+
+function Country3(){
+    console.log("Turkey, TUR")
+   }
+Country3("Turkey, TUR")
+
+
+function Country4(){
+    console.log("Russia, RUS")
+   }
+Country4("Russia, RUS")
+
+
+function Country5(){
+    console.log("Ukraine, UKR")
+   }
+
+   
+Country5("Ukraine, UKR")
+
+
+
+
+function add(a, b){
+    console.log(a + b)
+    console.log(a + " + " + b  +  " = ",  a+b);
+}
+
+add(2,3)
+
+
+
+function add(a, b){
+    console.log(a + b)
+    console.log(a + " + " + b  +  " = ",  a+b);
+}
+
+add(5,10)
+
+
+
+
+function add(a, b){
+    console.log(a + b)
+    console.log(a + " + " + b  +  " = ",  a+b);
+}
+
+add(12,8)
+
+// Assignment
+
+function subtract(a, b){
+    console.log(a - b)
+    console.log(a + " - " + b + " = ", a-b)
+}
+
+subtract(70,13)
+
+function subtract(a, b){
+    console.log(a - b)
+    console.log(a + " - " + b  +  " = ",  a-b);
+}
+
+subtract(10,2)
+
+function multiply(a, b){
+    console.log(a * b)
+    console.log(a + " * " + b  +  " = ",  a*b);
+}
+
+multiply(2,3)
+
+function multiply(a, b){
+    console.log(a*b)
+    console.log(a + " * " + b + " = ", a*b);
+}
+
+multiply(5,12)
